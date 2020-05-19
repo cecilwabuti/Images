51f715c867bb373c8f8981d63b201536b7e59aa8
cwabuti <cecilwabuti@gmail.com>
2020-05-19 11:39:19 +0300

    this the same app

---
+++ app/src/main/java/com/cec/images/Gallery/FullImageActivity.java
@@ -0,0 +1,30 @@
+package com.cec.images.Gallery;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+import com.cec.images.R;
+
+import java.util.Objects;
+
+public class FullImageActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery_full_scale);
+
+        // get intent data
+        Intent i = getIntent();
+
+        // Selected image id
+        int position = Objects.requireNonNull(i.getExtras()).getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        ImageView imageView = findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.mThumbIds[position]);
+    }
+    
+}
---
+++ app/src/main/java/com/cec/images/Gallery/ImageMainActivity.java
@@ -0,0 +1,76 @@
+package com.cec.images.Gallery;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+
+import com.cec.images.R;
+
+public class ImageMainActivity extends Activity implements MediaScannerConnection.MediaScannerConnectionClient {
+    public String[] allFiles;
+    private String SCAN_PATH ;
+    private static final String FILE_TYPE = "*/*";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery_grid_layout);
+//        getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+
+//        variable declaration
+
+        GridView gridViewP = findViewById(R.id.gridViewP);
+        GridView gridViewE = findViewById(R.id.gridViewE);
+
+        // Instance of ImageAdapter Class
+        gridViewP.setAdapter(new ImageAdapter(this));
+        gridViewE.setAdapter(new ImageAdapter(this));
+
+
+        /*
+          On Click event for Single Gridview Item
+          */
+        gridViewE.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                // Sending image id to FullScreenActivity
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                // passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+        gridViewP.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                // Sending image id to FullScreenActivity
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                // passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    public void onMediaScannerConnected() {
+
+    }
+
+    @Override
+    public void onScanCompleted(String path,Uri uri) {
+
+    }
+}
+
